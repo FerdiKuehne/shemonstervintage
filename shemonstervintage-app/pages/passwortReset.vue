272d3a90dec3a1f0cdf1c62130e1823a399e1c04
tonek2k <m.behrens@evalues.io>
2025-10-04 18:16:48 +0200

    changes

--- shemonstervintage-app/pages/passwortReset.vue
+++ shemonstervintage-app/pages/passwortReset.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="pwforgot-wrapper">
+    <div class="pwforgot">
     <h3 class="pr_title">{{ $t("passwordReset.title") }}</h3>
     <div class="pr_subtitle">
         {{ $t("passwordReset.subtitle") }}
@@ -19,6 +20,7 @@
     <button @click="passwordReset" type="submit">{{ $t("passwordReset.submit") }}</button>
 
   </div>
+ </div>
 </template>
 
 <script setup>
@@ -63,41 +65,18 @@ const passwordReset = async () => {
 
 <style scoped>
 
-.cofirmation-input-group {
-  position: relative;
-  margin-bottom: 1.5rem;
+.pwforgot-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
 }
 
-.cofirmation-input {
-  font-size: 16px;
-  padding: 10px 10px 10px 5px;
-  display: block;
-  width: 200px;
-  outline: none;
-  border: none;
-  border-bottom: 1px solid #515151;
-  background: transparent;
-}
-
-.cofirmation-label {
-  position: absolute;
-  pointer-events: none;
-  top: 10px;
-  left: 5px;
-  transition: top 1s ease, font-size 0.6s ease, color 0.4s ease;
-}
-
-.cofirmation-input:focus ~ .cofirmation-label,
-.cofirmation-input:not(:placeholder-shown) ~ .cofirmation-label  {
-  top: -10px;
-  font-size: 14px;
-  color: #515151;
-}
-
-.cofirmation-input:focus {
-  border-width: 2px;
-  border-color: #515151;
-  box-shadow: inset 0 -3px 5px -3px #fffefe; /* only bottom */
+.pwforgot {
+  width: 400px;
+  border: 1px solid #000;
+  padding: 1rem;
 }
 
 </style>
