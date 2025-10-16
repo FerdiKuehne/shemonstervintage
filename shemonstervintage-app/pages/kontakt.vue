12ca6ceb2af85571d35cb6fc1b3e43a9c1f7950a
tonek2k <m.behrens@evalues.io>
2025-10-16 20:07:35 +0200

    changes

--- shemonstervintage-app/pages/kontakt.vue
+++ shemonstervintage-app/pages/kontakt.vue
@@ -4,13 +4,13 @@
       <div class="page-headline">CONTACT</div>
     </div>
     <div>
-      <div class="container mt-5">
+      <div class="container">
         <div class="row">
           <div
             class="col-12 col-sm-12 col-md-10 col-lg-6 offset-0 offset-sm-0 offset-md-1 offset-lg-3"
           >
             <div class="text-start">
-              <h2 class="mt-4 mb-4 bold">{{ $t("nav.kontakt") }}</h2>
+              <!--<h2 class="mt-4 mb-4 bold">{{ $t("nav.kontakt") }}</h2>-->
             </div>
           </div>
         </div>
